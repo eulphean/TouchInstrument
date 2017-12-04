9162f65ea605f4e6a58c981b088e99676be3e1c5
Amay Kataria <amkatari@microsoft.com>
2017-12-04 13:05:57 -0600

    Code complete on Sending and Receiving Video signals by Touch OSC.

--- OpenFrameworks/src/Osc.cpp
+++ OpenFrameworks/src/Osc.cpp
@@ -17,7 +17,15 @@ void Osc::update() {
     // Notes can range from 0 - 127. Make sure no two note
     // numbers are same.
     
-    // Kick off Scene 1
+    processAudioMessages(m);
+    
+    // We receive these and route them directly to Mac Mini.
+    processVideoMessages(m);
+  }
+}
+
+void Osc::processAudioMessages(ofxOscMessage &m) {
+      // Kick off Scene 1
     if (m.getAddress() == "/Scenes/4/1") {
       int val = m.getArgAsInt(0);
       if (val == 1) {
@@ -111,5 +119,81 @@ void Osc::update() {
     if (m.getAddress() == "/Scenes/fader5") {
       float val = m.getArgAsFloat(0);
     }
+}
+
+// Route these messages directly to Mac mini.
+void Osc::processVideoMessages(ofxOscMessage &m) {
+  if(m.getAddress() == "/Video/stripes/blend"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/stripes/offset"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/stripes/rotation"){
+      sender.sendMessage(m);
+  } // Global Mixer
+  else if(m.getAddress() == "/Video/global/noise"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/global/stripes"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/global/tree"){
+      sender.sendMessage(m);
+  } // KSMR FX.
+  else if(m.getAddress() == "/Video/ksmr/noise"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/edge"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/fringe"){
+      sender.sendMessage(m);  }
+  else if(m.getAddress() == "/Video/ksmr/invert"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/slant"){
+     sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/tex"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/vertN"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/vertS"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/water"){
+      sender.sendMessage(m);
+  } // Uniform volume parameters for KSMR FX.
+  else if(m.getAddress() == "/Video/ksmr/noiseVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/edgeVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/fringeVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/invertVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/slantVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/texVolume"){
+      sender.sendMessage(m);  }
+  else if(m.getAddress() == "/Video/ksmr/vertnVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/vertsVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/ksmr/waterVolume"){
+      sender.sendMessage(m);
+  }
+  else if(m.getAddress() == "/Video/reset"){
+      sender.sendMessage(m);
   }
 }
--- OpenFrameworks/src/Osc.h
+++ OpenFrameworks/src/Osc.h
@@ -3,13 +3,20 @@
 #include "ofxOsc.h"
 
 #define PORT 8000
+#define HOST "10.73.36.26" // Update this IP address to the IP of the other MAC.
+#define PORTTOSEND 12345
 
 class Osc {
   public:
     void setup();
     void update();
+    void processAudioMessages(ofxOscMessage &m);
+    void processVideoMessages(ofxOscMessage &m);
     
   private:
     // Touch OSC parameters.
     ofxOscReceiver receive;
+  
+    // To send OSC messages to another machine.
+    ofxOscSender sender;
 };
