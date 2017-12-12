516ba861267fd0b01a3b8aa804da37b24fb62fc2
Amay Kataria <amkatari@microsoft.com>
2017-12-12 16:31:42 -0600

    Add Touch Oscillator files to the project.

---
+++ ParabolicSound/src/TouchOscillator.h
@@ -0,0 +1,33 @@
+// Author: Amay Kataria
+// Date: 10/16/2017
+// Description: Declares a TouchOscillator class, which is responsible to play an oscillating
+// sample when a connection happens. This could be a sin, square or a triangle wave.
+
+#pragma once
+
+#include "ofxPDSP.h"
+
+class TouchOscillator : public pdsp::Patchable{
+    public:
+        TouchOscillator();
+        TouchOscillator(const TouchOscillator & other) { patch(); }
+        void patch();
+    
+        // Inputs
+        pdsp::Patchable& in_trig();
+        pdsp::Patchable& in_pitch();
+    
+        // Outputs
+        pdsp::Patchable& out_sin();
+        pdsp::Patchable& out_saw();
+        pdsp::Patchable& out_triangle();
+        pdsp::Patchable& out_pulse();
+    
+    // Characteristics of this oscillator. 
+    private:
+        pdsp::Amp amp;
+        pdsp::VAOscillator osc;
+        pdsp::ADSR env;
+        pdsp::CRSlew ampSlew;
+        pdsp::CRSlew pitchSlew;
+};
